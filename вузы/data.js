ad7670b9daa46cc0e57b9a0c1e94b31d64502263
Aset41 <153676393+Aset41@users.noreply.github.com>
2025-05-09 02:44:19 +0500

    Add files via upload

---
+++ вузы/data.js
@@ -0,0 +1,24 @@
+const universities = [
+  { 
+      name: "Назарбаев Университет", 
+      location: "Нур-Султан", 
+      description: "Ведущий университет Казахстана с международными стандартами образования.", 
+      website: "https://nu.edu.kz",
+      logo: "img/universities/kaznu.png"
+  },
+  { 
+      name: "КазНУ им. аль-Фараби", 
+      location: "Алматы", 
+      description: "Крупнейший университет Казахстана с богатой историей и традициями.", 
+      website: "https://kaznu.kz",
+      logo: "img/universities/казну.png"
+  },
+  // ... остальные университеты (вставьте все остальные из предыдущего примера)
+  { 
+      name: "Атырауский университет нефти и газа", 
+      location: "Атырау", 
+      description: "Специализируется на подготовке кадров для нефтегазовой отрасли.", 
+      website: "https://aup.edu.kz",
+      logo: "img/universities/aup.png"
+  }
+];
